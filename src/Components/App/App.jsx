404bc49435e12e3953f2606fc62916a4542c0107
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-06-10 16:49:18 +0530

    defined all the routes

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./../Home/Home";
+import About from "./../About/About";
+import Projects from "./../Projects/Projects";
+import Skills from "./../Skills/Skills";
+import Contact from "./../Contact/Contact";
 import './App.css';
 
 class App extends React.Component {
@@ -6,9 +12,17 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1> Hello World!!</h1>
-              
-<a class="github-button" href="https://github.com/jyoti-2" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @jyoti-2 on GitHub">Follow @jyoti-2</a>
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/projects" component={Projects} />
+                        <Route path="/skills" component={Skills} />
+                        <Route path="/contact" component = {Contact}/>
+                    </div>
+                </BrowserRouter>
+                
+
             </div>
         );
     };
